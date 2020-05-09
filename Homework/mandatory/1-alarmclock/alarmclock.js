8dce310a5dd47fb7b311f8cf2d1ebfc5eefcd137
Esteban <emedina@creatonico.com>
2020-05-09 18:23:07 +0200

    updated Alarmclock Exercise code

--- Homework/mandatory/1-alarmclock/alarmclock.js
+++ Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,17 +1,45 @@
+let alarmSeconds;
+let alarmInterval;
+let millisPerSecond = 1000;
+
 function setAlarm() {
   let alarmInput = document.querySelector("#alarmSet");
-  let timeRemaining = document.querySelector("#timeRemaining");
-  let countDown = alarmInput.value;
-  timeRemaining.innerText = "Time Remaining: 00:" + countDown;
-  let setTimeRemaining = function () {
-    timeRemaining.innerText = "Time Remaining: 00:" + --countDown;
-    if (countDown === 0) {
-      clearInterval(setTimeRemainingId);
-      playAlarm();
-      document.body.style.backgroundColor = "yellow";
-    }
-  };
-  var setTimeRemainingId = setInterval(setTimeRemaining, 1000);
+  alarmSeconds = alarmInput.value;
+  alarmInterval = setInterval(subtractAlarmSecond, millisPerSecond);
+  displayTimeRemaining();
+}
+
+function subtractAlarmSecond() {
+  alarmSeconds--;
+  displayTimeRemaining();
+  if (alarmSeconds === 0) {
+    clearInterval(alarmInterval);
+    playAlarm();
+  }
+}
+
+function displayTimeRemaining() {
+  let timeRemainingH1 = document.querySelector("#timeRemaining");
+
+  let minutes = Math.floor(alarmSeconds / 60);
+  let seconds = alarmSeconds - minutes * 60;
+
+  let minutesString = "";
+  if (minutes < 10) {
+    minutesString = "0" + minutes;
+  } else {
+    minutesString = minutes;
+  }
+
+  let secondsString = "";
+  if (seconds < 10) {
+    secondsString = "0" + seconds;
+  } else {
+    secondsString = seconds;
+  }
+
+  timeRemainingH1.innerText =
+    "Time Remaining: " + minutesString + ":" + secondsString;
 }
 
 // DO NOT EDIT BELOW HERE
