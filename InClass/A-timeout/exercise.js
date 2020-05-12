ba1e2829e0dfd1e7489bb991aeca6e4ee4b2a0c7
Esteban <emedina@creatonico.com>
2020-05-12 20:10:43 +0200

    updated A-timeout exercise

--- InClass/A-timeout/exercise.js
+++ InClass/A-timeout/exercise.js
@@ -11,26 +11,21 @@ Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_se
 ================
 */
 
+/*
+let bg = document.querySelector("body");
+function changeColor() {
+  bg.style.backgroundColor = "red";
+}
+setTimeout(changeColor, 2000);
+*/
+
 let background = document.querySelector("body");
-let colors = ["red", "blue", "green" ]
+let colors = ["red", "green", "blue"];
 let counter = 0;
-setInterval(function) {
-    
-}
-
-
-function setbackground() {
-  window.setTimeout("setbackground()", 5000);
-  var index = Math.round(Math.random() * 9);
-  var ColorValue = "#E6A9EC";
-  if (index == 1) ColorValue = "FFCCCC";
-  if (index == 2) ColorValue = "CCAFFF";
-  if (index == 3) ColorValue = "A6BEFF";
-  if (index == 4) ColorValue = "99FFFF";
-  if (index == 5) ColorValue = "D5CCBB";
-  if (index == 6) ColorValue = "99FF99";
-  if (index == 7) ColorValue = "FFFF99";
-  if (index == 8) ColorValue = "FFCC99";
-  if (index == 9) ColorValue = "CCCCCC";
-  document.bgColor = ColorValue;
-}
+setInterval(function () {
+  background.style.backgroundColor = colors[counter];
+  counter++;
+  if (counter > 2) {
+    counter = 0;
+  }
+}, 2000);
